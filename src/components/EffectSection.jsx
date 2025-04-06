afc7f6ceaa260766d76e15ad0fa02dd4e379344a
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-04-06 20:32:13 +0300

    создаём компонент модального окна

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,7 +1,19 @@
+import Button from "./Button/Button.jsx";
+
 export default function EffectSection() {
+
+  function openModal() {
+    
+  }
+
+
   return (
     <section>
       <h3>Effect</h3>
+
+      <Button onClick = {openModal}>
+        Открыть информацию
+      </Button>
     </section>
   )
 }
