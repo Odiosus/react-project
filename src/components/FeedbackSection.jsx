0d315deaa711ae6d98473c191eec17f8d2b73970
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-04-06 17:11:14 +0300

    добавлена форма

--- src/components/FeedbackSection.jsx
+++ src/components/FeedbackSection.jsx
@@ -1,7 +1,32 @@
+import Button from "./Button/Button.jsx";
+
 export default function FeedbackSection() {
-  return(
+
+
+  return (
     <section>
-      <h3 className="h1">Feedback</h3>
+      <h3 className = "h1">Feedback</h3>
+
+      <form action = "">
+        <label htmlFor = "name">Ваше имя</label>
+        <input
+          type = "text"
+          className = "control"
+          id = "name"
+        />
+
+        <label htmlFor = "reason">Причина образения</label>
+        <select
+          id = "reason"
+          className = "control"
+        >
+          <option value = "error">Ошибка</option>
+          <option value = "help">Нужна помощь</option>
+          <option value = "sugest">Предложение</option>
+        </select>
+
+        <Button/>
+      </form>
     </section>
   )
 }
