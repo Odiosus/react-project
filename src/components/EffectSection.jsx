44162ad9cf5b77c7fe7fd1ab9f2927853c003fe6
Suchkov Maksim <barbell.wire0b@icloud.com>
2025-04-07 15:07:51 +0300

    добавлен стэйт загрузки и полчуения списка юзеров, колбэк с запросом на получение списка юзкров на сервер. выводим списком на страницу

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,22 +1,45 @@
 import Button from "./Button/Button.jsx";
 import Modal from "./Modal/Modal.jsx";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function EffectSection() {
 
   // состояние модального окна по умолчанию — скрыто (false)
-  const [isModalOpen, setIsModalOpen] = useState( false )
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  // состояние загрузки, по умолчанию false
+  const [loading, setLoading] = useState(false)
+  // состояние получения списка юзеров (пустой массив)
+  const [users, setUsers] = useState([])
 
-  // изменение состояния модального окна — отображено (true)
+
+  // переменная колбэк с запросом получения списка юзеров
+  const fetchUsers = useCallback(async() => {
+    setLoading(true)
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await response.json()
+    setUsers(users)
+    setLoading(false)
+  }, []);
+
+  // запрашиваем и обновляем переменную со списком юзеров
+  useEffect(() => {
+    fetchUsers()
+  }, [fetchUsers]);
+
+  /*// (перенсено в onClick) изменение состояния модального окна — отображено (true)
   function openModal() {
     setIsModalOpen( true )
-  }
+  }*/
 
   return (
     <section>
       <h3>Effect</h3>
 
-      <Button onClick = {openModal}>
+      <Button onClick = {() => {
+        setIsModalOpen(true)
+      }}
+        style = {{marginBottom:'1rem'}}
+      >
         Открыть информацию
       </Button>
 
@@ -27,10 +50,16 @@ export default function EffectSection() {
           impedit ipsum iusto labore laboriosam magnam necessitatibus nemo, nesciunt nulla numquam quasi recusandae
           repellendus, repudiandae!
         </p>
-        <Button onClick = {() => setIsModalOpen( false )}>
+        <Button
+          onClick = {() => setIsModalOpen(false)}>
           Close modal
         </Button>
       </Modal>
+
+      {loading && <p>Loading...</p>}
+      {!loading && <ul>
+        {users.map(user => <li key = {user.id}>{user.name}</li>)}
+      </ul>}
     </section>
   )
 }
